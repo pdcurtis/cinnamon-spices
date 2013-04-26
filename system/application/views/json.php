767ceb628c72530b40bd3083ea05d1624a4e236f
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2013-04-26 14:47:31 +0100

    Don't compress the JSON! :)

--- system/application/views/json.php
+++ system/application/views/json.php
@@ -1,44 +1,28 @@
 <?php
-    header("Content-Type: application/json");
     
-    $pack = true;
-    if(!empty($_SERVER["HTTP_ACCEPT_ENCODING"]) && strpos("gzip",$_SERVER["HTTP_ACCEPT_ENCODING"]) === NULL) {
-        $pack = false;
-    }
+    $json = array();
 
-    $list = array();
-
-    if ($spiceData->num_rows > 0) {
+    if ($spiceData->num_rows() > 0) {
         foreach ($spiceData->result() as $spice) {
-            $list[] = array(
-                'spices-id'     => $spice->id,
-                'uuid'          => $spice->uuid,
-                'name'          => $spice->name,
-                'description'   => $spice->description,
-                'score'         => $spice->score,
-                'created'       => $spice->created,
-                'last_edited'   => $spice->last_edited,
-                'file'          => $spice->file,
-                'icon'          => $spice->icon,
-                'screenshot'    => $spice->screenshot,
-                'author_id'     => $spice->user,
-                'author_user'   => $spice->username,
-                'website'       => $spice->website,
+            $json[$spice->uuid] = array(
+                'spices-id' => $spice->id,
+                'uuid' => $spice->uuid,
+                'name' => $spice->name,
+                'description' => $spice->description,
+                'score' => $spice->score,
+                'created' => $spice->created,
+                'last_edited' => $spice->last_edited,
+                'file' => $spice->file,
+                'icon' => $spice->icon,
+                'screenshot' => $spice->screenshot,
+                'author_id' => $spice->user,
+                'author_user' => $spice->username,
             );
         }
     }
 
-    $json = json_encode($list);
-
-    if ($pack) {
-        header("Content-Encoding: gzip");
-        $output = gzencode($json,9,true);
-    }
-    else {
-        $output = $json;
-    }
-
-    header("Content-Length: " . mb_strlen($output, 'latin1'));
-
-    echo $output;
+    $response = json_encode($json);
+    header("Content-type: application/json");
+    header("Content-length: ".strlen($response) . "\r\n");
+    exit($response);
 ?>
