0d4c92e0571a722ccbc49c668c00028e79391b4b
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2013-04-26 23:22:35 +0100

    Fixed JSON for themes

--- system/application/views/json.php
+++ system/application/views/json.php
@@ -4,20 +4,36 @@
 
     if ($spiceData->num_rows() > 0) {
         foreach ($spiceData->result() as $spice) {
-            $json[$spice->uuid] = array(
-                'spices-id' => $spice->id,
-                'uuid' => $spice->uuid,
-                'name' => $spice->name,
-                'description' => $spice->description,
-                'score' => $spice->score,
-                'created' => $spice->created,
-                'last_edited' => $spice->last_edited,
-                'file' => $spice->file,
-                'icon' => $spice->icon,
-                'screenshot' => $spice->screenshot,
-                'author_id' => $spice->user,
-                'author_user' => $spice->username,
-            );
+            if ($spiceType == "themes") {
+                $json[$spice->uuid] = array(
+                    'spices-id' => $spice->id,                    
+                    'name' => $spice->name,
+                    'description' => $spice->description,
+                    'score' => $spice->score,
+                    'created' => $spice->created,
+                    'last_edited' => $spice->last_edited,
+                    'file' => $spice->file,                    
+                    'screenshot' => $spice->screenshot,
+                    'author_id' => $spice->user,
+                    'author_user' => $spice->username
+                );
+            }
+            else {
+                $json[$spice->uuid] = array(
+                    'spices-id' => $spice->id,
+                    'uuid' => $spice->uuid,
+                    'name' => $spice->name,
+                    'description' => $spice->description,
+                    'score' => $spice->score,
+                    'created' => $spice->created,
+                    'last_edited' => $spice->last_edited,
+                    'file' => $spice->file,
+                    'icon' => $spice->icon,
+                    'screenshot' => $spice->screenshot,
+                    'author_id' => $spice->user,
+                    'author_user' => $spice->username
+                );
+            }
         }
     }
 
