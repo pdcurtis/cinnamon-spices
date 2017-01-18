e456718b7b893028d98c327f808e382f43f3bdf9
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2017-01-18 14:03:30 +0000

    Remove some trailing spaces

--- system/application/controllers/themes.php
+++ system/application/controllers/themes.php
@@ -5,13 +5,13 @@ class Themes extends Controller{
 		parent::Controller();
 		$this->load->helper('url');
 		$this->load->helper('form');
-		$this->load->helper('date');	
-		$this->load->library('email');	
-		
+		$this->load->helper('date');
+		$this->load->library('email');
+
 		error_reporting(E_ALL);
-		ini_set('display_errors', '1');		
+		ini_set('display_errors', '1');
 	}
-	
+
 	function index() {
 		$this->popular();
 	}
@@ -23,7 +23,7 @@ class Themes extends Controller{
 //		$this->load->view('themes', $data);
 //		$this->load->view('footer', $data);
 //	}
-	
+
 	function popular() {
 		$this->db->order_by('score DESC, name ASC');
 		$data['mode'] = 'popular';
@@ -41,7 +41,7 @@ class Themes extends Controller{
 //		$this->load->view('themes', $data);
 //		$this->load->view('footer', $data);
 //	}
-	
+
 	function latest()
 	{
 		$this->db->order_by('last_edited DESC, name ASC');
@@ -52,22 +52,22 @@ class Themes extends Controller{
 		$this->load->view('themes', $data);
 		$this->load->view('footer', $data);
 	}
-	
+
 	function view($id) {
 		$id = intval($id);
-		$this->db->select('themes.*, users.id as user_id, users.username, users.signature, users.biography');		
-		$this->db->where('themes.id', $id);		
+		$this->db->select('themes.*, users.id as user_id, users.username, users.signature, users.biography');
+		$this->db->where('themes.id', $id);
 		$this->db->join('users', 'users.id = themes.user');
 		$records = $this->db->get('themes');
 		if ($records->num_rows() > 0) {
 			$data = $records->row_array();
-			
+
 			$this->db->select('themes_comments.*, users.username');
 			$this->db->where('themes_comments.theme', $id);
 			$this->db->join('users', 'users.id = themes_comments.user');
 			$this->db->order_by('timestamp DESC');
 			$data['comments'] = $this->db->get('themes_comments');
-			
+
 			$data['rating'] = 0;
 			if ($this->dx_auth->is_logged_in()) {
 				$this->db->where('user', $this->dx_auth->get_user_id());
@@ -77,7 +77,7 @@ class Themes extends Controller{
 					$data['rating'] = $records->row()->rating;
 				}
 			}
-									
+
 			$this->load->view('header_short');
 			$this->load->view('theme', $data);
 			$this->load->view('footer');
@@ -90,11 +90,11 @@ class Themes extends Controller{
 			$this->load->view("footer");
 		}
 	}
-	
+
 	function generatethumbs() {
 		$this->createThumbs(FCPATH.'uploads/themes/',FCPATH.'uploads/themes/thumbs/',100);
 	}
-	
+
 	function createThumbs( $pathToImages, $pathToThumbs, $thumbWidth )
 	{
 	  // open the directory
@@ -169,78 +169,78 @@ class Themes extends Controller{
 		fwrite($fp, json_encode($json));
 		fclose($fp);
 	}
-	
+
 	function rate($id, $rating) {
 		$id = intval($id);
 		$rating = intval($rating);
-		if ($this->dx_auth->is_logged_in() && $rating >= 1 && $rating <= 5) {    			
+		if ($this->dx_auth->is_logged_in() && $rating >= 1 && $rating <= 5) {
 			$this->db->where('user', $this->dx_auth->get_user_id());
 			$this->db->where('theme', $id);
-			$this->db->delete('themes_ratings');						
+			$this->db->delete('themes_ratings');
 			$this->db->set('user', $this->dx_auth->get_user_id());
 			$this->db->set('theme', $id);
 			$this->db->set('rating', $rating);
-			$this->db->insert('themes_ratings');			
+			$this->db->insert('themes_ratings');
 			$this->db->query("UPDATE themes SET score = (SELECT SUM(rating-3) FROM themes_ratings WHERE theme = $id) WHERE id = $id");
 			$this->_json();
 		}
 		redirect("/themes/view/$id", "location");
 	}
-	
+
 	function certify($id, $certification) {
 		$id = intval($id);
 		$certification = intval($certification);
-		if ($this->dx_auth->is_logged_in() && $this->dx_auth->is_admin()) {    														
+		if ($this->dx_auth->is_logged_in() && $this->dx_auth->is_admin()) {
 			$this->db->where('id', $id);
 			$this->db->set('certification', $certification);
-			$this->db->update('themes');	
-			$this->_json();		
+			$this->db->update('themes');
+			$this->_json();
 		}
 		redirect("/themes/view/$id", "location");
 	}
-	
+
 	// function edit($id, $error="") {
 	// 	$id = intval($id);
-	// 	if ($this->dx_auth->is_logged_in()) {			
-	// 		$this->db->where('id', $id);					
+	// 	if ($this->dx_auth->is_logged_in()) {
+	// 		$this->db->where('id', $id);
 	// 		$this->db->where('user', $this->dx_auth->get_user_id());
 	// 		$records = $this->db->get('themes');
-	// 		if ($records->num_rows() > 0) {	
+	// 		if ($records->num_rows() > 0) {
 	// 			$data = $records->row_array();
 	// 			$data['error'] = $error;
 	// 			$this->load->view('header');
 	// 			$this->load->view('theme_edit', $data);
 	// 			$this->load->view('footer');
-	// 		}			
-	// 	}		
+	// 		}
+	// 	}
 	// }
-	
+
 	// function edit_save($id) {
 	// 	$id = intval($id);
- //        if ($this->dx_auth->is_logged_in()) {    
-	// 		$this->db->where('id', $id);					
+ //        if ($this->dx_auth->is_logged_in()) {
+	// 		$this->db->where('id', $id);
 	// 		$this->db->where('user', $this->dx_auth->get_user_id());
 	// 		$records = $this->db->get('themes');
-	// 		if ($records->num_rows() > 0) {	
+	// 		if ($records->num_rows() > 0) {
 	// 			$data = $records->row_array();
-			
-	// 			$this->db->where('id', $id);					
+
+	// 			$this->db->where('id', $id);
 	// 			$this->db->where('user', $this->dx_auth->get_user_id());
 	// 			$this->db->set('name', $_POST['name']);
 	// 			$this->db->set('version', $_POST['version']);
 	// 			$this->db->set('website', $_POST['website']);
-	// 			$this->db->set('description', $_POST['description']);				
+	// 			$this->db->set('description', $_POST['description']);
 	// 			$this->db->set('last_edited', now());
-	// 			$this->db->update('themes');	
-				
+	// 			$this->db->update('themes');
+
 	// 			$this->load->library('upload');
-	// 			if ($_FILES['file']['error'] != 4) {	
-	// 				#Upoad the new file										
+	// 			if ($_FILES['file']['error'] != 4) {
+	// 				#Upoad the new file
 	// 				$filename = $this->genRandomString();
 	// 				$config['upload_path'] = FCPATH.'uploads/themes/';
 	// 				$config['file_name'] = $filename;
 	// 				$config['overwrite'] = TRUE;
-	// 				$config['allowed_types'] = 'zip';				
+	// 				$config['allowed_types'] = 'zip';
 	// 				$config['max_size']	= '10240';
 
 	// 				$this->upload->initialize($config);
@@ -265,14 +265,14 @@ class Themes extends Controller{
 	// 					$this->db->update('themes');
 	// 				}
 	// 			}
-				
+
 	// 			if ($_FILES['screenshot']['error'] != 4){
-	// 				#Upoad the new file										
+	// 				#Upoad the new file
 	// 				$filename = $this->genRandomString();
 	// 				$config['upload_path'] = FCPATH.'uploads/themes/';
 	// 				$config['file_name'] = $filename;
 	// 				$config['overwrite'] = TRUE;
-	// 				$config['allowed_types'] = 'png';				
+	// 				$config['allowed_types'] = 'png';
 	// 				$config['max_size']	= '1024';
 
 	// 				$this->upload->initialize($config);
@@ -300,115 +300,115 @@ class Themes extends Controller{
 	// 			}
 
 	// 			die;
-	// 			$this->_json();					
-	// 		}	                                            					                                                
+	// 			$this->_json();
+	// 		}
  //        }
  //        redirect("/themes/view/$id", "location");
  //    }
-	
+
 	// function delete($id) {
 	// 	$id = intval($id);
-	// 	if ($this->dx_auth->is_logged_in()) {			
-	// 		$this->db->where('id', $id);					
+	// 	if ($this->dx_auth->is_logged_in()) {
+	// 		$this->db->where('id', $id);
 	// 		$this->db->where('user', $this->dx_auth->get_user_id());
 	// 		$records = $this->db->get('themes');
-	// 		if ($records->num_rows() > 0) {	
+	// 		if ($records->num_rows() > 0) {
 	// 			$data = $records->row_array();
-				
+
 	// 			#Delete the files
-	// 			unlink(FCPATH.$data['screenshot']);						
-	// 			unlink(FCPATH.$data['file']);						
-				
+	// 			unlink(FCPATH.$data['screenshot']);
+	// 			unlink(FCPATH.$data['file']);
+
 	// 			$this->db->where('theme', $id);
 	// 			$this->db->delete('themes_comments');
-											
+
 	// 			$this->db->where('id', $id);
 	// 			$this->db->where('user', $this->dx_auth->get_user_id());
 	// 			$this->db->delete('themes');
 
 	// 			$this->_json();
-	// 		}		
+	// 		}
 	// 	}
 	// 	redirect("/themes", "location");
 	// }
-	
+
 	function comment($id) {
 		$id = intval($id);
 		if ($this->dx_auth->is_logged_in()) {
 			$this->db->set('user', $this->dx_auth->get_user_id());
 			$this->db->set('theme', $id);
 			$this->db->set('timestamp', now());
-			$this->db->set('body', $_POST['body']);	
+			$this->db->set('body', $_POST['body']);
 			$this->db->insert('themes_comments');
 		}
 		redirect("/themes/view/$id", "location");
 	}
-			
+
 	// function create_new($error="") {
-	// 	if ($this->dx_auth->is_logged_in()) {	
-	// 		$data["error"] = $error;		
-	// 		$this->load->view('header');			
+	// 	if ($this->dx_auth->is_logged_in()) {
+	// 		$data["error"] = $error;
+	// 		$this->load->view('header');
 	// 		$this->load->view('themes_new', $data);
 	// 		$this->load->view('footer');
 	// 	}
 	// }
-		    
+
  //    function create_new_save() {
- //        if ($this->dx_auth->is_logged_in()) {    
+ //        if ($this->dx_auth->is_logged_in()) {
 	// 		$this->load->library('form_validation');
 	// 		$this->form_validation->set_rules('name', 'Name', 'required');
 	// 		$this->form_validation->set_rules('description', 'Description', 'required');
-	// 		$this->form_validation->set_rules('version', 'Version', 'required');						
+	// 		$this->form_validation->set_rules('version', 'Version', 'required');
 	// 		$this->form_validation->set_rules('website', 'Website', 'required');
-			                                                   
+
  //            if ($this->form_validation->run() == FALSE) {
 	// 			$this->create_new();
 	// 			return;
 	// 		}
-			
+
 	// 		if ($_FILES['file']['error'] == 4) {
 	// 			$this->create_new("A file is required");
 	// 			return;
 	// 		}
-			
+
 	// 		if ($_FILES['screenshot']['error'] == 4){
 	// 			$this->create_new("A screenshot is required");
 	// 			return;
-	// 		}			
-			
+	// 		}
+
 	// 		$this->load->library('upload');
-			
+
 	// 		// Upload the file
 	// 		$filename = $this->genRandomString();
 	// 		$config['upload_path'] = FCPATH.'uploads/themes/';
 	// 		$config['file_name'] = $filename;
 	// 		$config['overwrite'] = TRUE;
-	// 		$config['allowed_types'] = 'zip';				
-	// 		$config['max_size']	= '10240';				
+	// 		$config['allowed_types'] = 'zip';
+	// 		$config['max_size']	= '10240';
 	// 		$this->upload->initialize($config);
-										
-	// 		if (!$this->upload->do_upload('file')) {					
+
+	// 		if (!$this->upload->do_upload('file')) {
 	// 			$this->create_new($this->upload->display_errors());
 	// 			return;
 	// 		}
-	// 		else {										
+	// 		else {
 	// 			$upload_data = $this->upload->data();
 	// 			$file_file_name = $upload_data['file_name'];
-	// 			chmod( FCPATH.'uploads/themes/'.$file_file_name , 0644 );  					
-				
+	// 			chmod( FCPATH.'uploads/themes/'.$file_file_name , 0644 );
+
 	// 			$config['upload_path'] = FCPATH.'uploads/themes/';
 	// 			$config['file_name'] = $filename;
 	// 			$config['overwrite'] = TRUE;
-	// 			$config['allowed_types'] = 'png';				
-	// 			$config['max_size']	= '1024';				
+	// 			$config['allowed_types'] = 'png';
+	// 			$config['max_size']	= '1024';
 	// 			$this->upload->initialize($config);
-				
-	// 			if (!$this->upload->do_upload('screenshot')) {			
+
+	// 			if (!$this->upload->do_upload('screenshot')) {
 	// 				//Delete theme file
-	// 				if (file_exists(FCPATH.'uploads/themes/'.$file_file_name)) {			 
+	// 				if (file_exists(FCPATH.'uploads/themes/'.$file_file_name)) {
 	// 					unlink(FCPATH.'uploads/themes/'.$file_file_name);
 	// 				}
-								       
+
 	// 				//Reload form
 	// 				$this->create_new($this->upload->display_errors());
 	// 				return;
@@ -416,8 +416,8 @@ class Themes extends Controller{
 	// 			else {
 	// 				$upload_data = $this->upload->data();
 	// 				$screenshot_file_name = $upload_data['file_name'];
-	// 				chmod( FCPATH.'uploads/themes/'.$screenshot_file_name , 0644 );  	
-					
+	// 				chmod( FCPATH.'uploads/themes/'.$screenshot_file_name , 0644 );
+
 	// 				$this->db->set("user", $this->dx_auth->get_user_id());
 	// 				$this->db->set("name", $_POST["name"]);
 	// 				$this->db->set("version", $_POST["version"]);
@@ -427,19 +427,19 @@ class Themes extends Controller{
 	// 				$this->db->set("screenshot", "/uploads/themes/".$screenshot_file_name);
 	// 				$this->db->set("created", now());
 	// 				$this->db->set("last_edited", now());
-	// 				$this->db->insert("themes");				
-	// 				$id = $this->db->insert_id();		
-					
+	// 				$this->db->insert("themes");
+	// 				$id = $this->db->insert_id();
+
 	// 				$this->generatethumbs();
 
 	// 				$this->_json();
-											
+
 	// 				redirect("/themes/view/$id", "location");
-	// 			}										
-	// 		}                                            					                                                  
+	// 			}
+	// 		}
  //        }
  //    }
-    
+
     function genRandomString() {
         $length = 10;
         $characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -454,9 +454,9 @@ class Themes extends Controller{
         $string .= '-';
         for ($p = 0; $p < 4; $p++) {
             $string .= $characters[mt_rand(0, strlen($characters) -1)];
-        }        
+        }
         return $string;
     }
-		
+
 }
 ?>
