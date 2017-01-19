553d997e73dd6a8cd2ed9a5265f4e65fbdd54488
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2017-01-19 14:48:34 +0000

    Port desklets and extensions to github

--- system/application/views/extensions.php
+++ system/application/views/extensions.php
@@ -1,20 +1,11 @@
 <div class="cs-content-filter">
     <ul>
-        <!--        <li class="active">-->
-        <!--            <a href="/themes/featured">Featured</a>-->
-        <!--        </li>-->
         <li class="<?= $mode == 'popular' ? 'active' : '' ?>">
             <a href="/extensions/popular">Popular</a>
         </li>
         <li class="<?= $mode == 'latest' ? 'active' : '' ?>">
             <a href="/extensions/latest">Latest</a>
         </li>
-        <!--        <li>-->
-        <!--            <a href="/themes/all">See All</a>-->
-        <!--        </li>-->
-        <!--        <li>-->
-        <!--            <a href="#">Upload your theme</a>-->
-        <!--        </li>-->
     </ul>
 </div>
 
