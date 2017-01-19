f2a112289ad87d3bc56f726d4a56e94555f50379
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2017-01-19 16:59:12 +0000

    Fixed footer on spices pages

--- system/application/views/footer_links.php
+++ system/application/views/footer_links.php
@@ -8,25 +8,23 @@
                 <li><a class="<?= preg_match('|^/desklets.*?|',$_SERVER['REQUEST_URI'])?'active':'' ?>" href="/desklets">Desklets</a></li>
                 <li><a class="<?= preg_match('|^/extensions.*?|',$_SERVER['REQUEST_URI'])?'active':'' ?>" href="/extensions">Extensions</a></li>
                 <?php if (!$this->dx_auth->is_logged_in()) { ?>
-                    <li><a class="<?= preg_match('|^/auth/login.*?|',$_SERVER['REQUEST_URI'])?'active':'' ?>" href="/auth/login">Log In</a></li>
-                    <li><a  class="<?= preg_match('|^/auth/register.*?|',$_SERVER['REQUEST_URI'])?'active':'' ?>" href="/auth/register">Sign Up</a></li>
+                    <!-- <li><a class="<?= preg_match('|^/auth/login.*?|',$_SERVER['REQUEST_URI'])?'active':'' ?>" href="/auth/login">Log In</a></li>
+                    <li><a  class="<?= preg_match('|^/auth/register.*?|',$_SERVER['REQUEST_URI'])?'active':'' ?>" href="/auth/register">Sign Up</a></li> -->
                 <?php } else { ?>
-                    <li><a href="/auth/logout">Log Out</a></li>
+                    <!-- <li><a href="/auth/logout">Log Out</a></li> -->
                 <?php } ?>
             </ul>
             <ul class="cs-footer-sub-navigation cs-inline-list">
-                <li><a href="#">Documentation</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Terms of Use</a></li>
+                <li><a href="http://developer.linuxmint.com/reference/git/cinnamon-tutorials/extension-system.html">Development</a></li>
+                <li><a href="http://github.com/linuxmint">Github</a></li>
             </ul>
         </div>
     </div>
     <div class="cs-footer-copyright cs-main-wrap cs-flex">
         <div class="cs-copyright">
-            Cinnamon is ©copyrighted 2011 and trademarked through the Linux Mark Institute. All rights reserved.
         </div>
         <div class="cs-sponsor">
-            Brought to you by <a class="cs-link-alternate" href="//www.linuxmint.com">LINUX MINT</a>
+            Brought to you by <a class="cs-link-alternate" href="https://www.linuxmint.com">LINUX MINT</a>
         </div>
     </div>
 </div>
