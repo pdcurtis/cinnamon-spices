0811b840951ae9f06fc0a20b13739fb4ac9412e2
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2017-01-20 10:58:31 +0000

    Theme: Remove obsolete certification code

--- system/application/views/theme.php
+++ system/application/views/theme.php
@@ -5,15 +5,6 @@
         padding: 0.25rem;
     }
 </style>
-<?php if ($this->dx_auth->is_logged_in() && $this->dx_auth->is_admin()) {
-    $certifications = $this->db->get("themes_certifications"); ?>
-    Certification:
-    <a href="<?= "/themes/certify/$id/0" ?>" class='cs-button-certify'>Un-certify</a>
-    <?php foreach ($certifications->result() as $certif) { ?>
-        <a href="<?= "/themes/certify/$id/$certif->id" ?>" class='cs-button-certify'><?= "Certify $certif->name" ?></a>
-    <?php } ?>
-    <hr>
-<?php } ?>
 
 <?php $this->view('_item_details_head',['type'=>'themes']) ?>
 
