d440ee21118351329329a1db1507f4610b6f2cfd
MrElectroNick <mr.electronick@gmail.com>
2017-01-21 23:55:28 +0200

    begin OAuth

--- system/application/views/theme.php
+++ system/application/views/theme.php
@@ -1,10 +1,3 @@
-<style>
-    .cs-button-certify {
-        font-size: 0.75rem;
-        border: solid 1px silver;
-        padding: 0.25rem;
-    }
-</style>
 
 <?php $this->view('_item_details_head',['type'=>'themes']) ?>
 
