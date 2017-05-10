2c07730f8e089dc93c61dd86c8b9f304c28f022d
nyrrad <dirtbikedb@gmail.com>
2017-05-10 12:34:44 -0500

    Reply indicator, Form clear button, Some slight css cleanups

--- comment.js
+++ comment.js
@@ -1,20 +1,35 @@
 (function() {
-    var container = document.getElementById('comments');
+    var container = document.getElementById('comments'),
+        form = document.getElementById('comment_form'),
+        clear = document.getElementById('form_clear'),
+        header = document.getElementById('form_header'),
+        parent_id = document.getElementById('parent_id');
+
+    clear.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        header.innerHTML = 'Leave A Comment.';
+        parent_id.setAttribute('value', '0');
+        form.reset();
+    });
 
     container.addEventListener('click', function(e) {
         var target = e.target;
 
         if (target.classList.contains('reply')) {
             var comment = target.getAttribute('data-id'),
-                parent = document.getElementById('parent_id');
+                name = target.getAttribute('data-name');
 
             e.preventDefault();
 
             // Move to comment form
             window.location.hash = 'comment-box';
 
+            // Indicate who commenter is responding to
+            header.innerHTML = 'Reply to ' + name + ',';
+
             // Set parent_id for reply post
-            parent.setAttribute('value', comment);
+            parent_id.setAttribute('value', comment);
         }
     });
 })();
--- system/application/views/_comments.php
+++ system/application/views/_comments.php
@@ -1,12 +1,16 @@
 <!-- START comments-box -->
 <div id="comment-box" class="cs-comment-box">
     <?php if ($this->session->userdata('oauth')) { ?>
-        <h2 class="cs-comments-title">Leave A Comment</h2>
+        <h2 id="form_header" class="cs-comments-title">Leave A Comment.</h2>
         <div class="cs-comment-form">
-            <?= form_open("$type/comment/$id") ?>
+            <?php
+                $attributes = array('id'=>'comment_form');
+                echo form_open("$type/comment/$id", $attributes);
+            ?>
             <textarea name="body" style="width:100%" rows="5" placeholder="comment here....."></textarea>
             <input name="parent_id" value="0" id="parent_id" style="display: none;"/>
             <input type="submit" class="cs-button cs-button-sm" value="Submit"/>
+            <a href="#" id="form_clear" class="cs-button cs-button-sm cs-button-cancel">Cancel</a>
             <?= form_close() ?>
         </div>
     <?php } else { ?>
