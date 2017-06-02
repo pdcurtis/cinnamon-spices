99fa883a660c19a383459b8701063ea3d809ed05
nyrrad <dirtbikedb@gmail.com>
2017-06-02 10:44:45 -0500

    wip ajax comments - start

--- index.js
+++ index.js
@@ -1,39 +1,76 @@
 (function() {
     // Check for comment-box
-    if (document.getElementById('comment_form')) {
+    var form = document.getElementById('comment-form-master');
+
+    function createForm(parent) {
+        var form_wrapper = document.createElement('div'),
+            new_form = document.createElement('form'),
+            send_button = document.createElement('a')
+            cancel_button = send_button.cloneNode(false),
+            new_textarea = document.createElement('textarea');
+
+        // form wrapper
+        form_wrapper.setAttribute('class', 'cs-comment-form cs-comment-child');
+        form_wrapper.setAttribute('id', 'comment-form-reply');
+
+        if (parent.getAttribute('data-depth') >= 2) {
+            form_wrapper.classList.add('cs-comment-no-indent');
+        }
+
+        // Send Button
+        send_button.setAttribute('class', 'cs-button cs-button-sm');
+        send_button.innerHTML = 'Submit';
+
+        // Cancel Button
+        cancel_button.setAttribute('class', 'cs-button cs-button-sm cs-button-cancel');
+        cancel_button.innerHTML = 'Cancel';
+
+        // Text box
+        new_textarea.setAttribute('name', 'reply_body');
+        new_textarea.setAttribute('rows', '4');
+        new_textarea.setAttribute('placeholder', 'Reply here.....');
+
+        // Build Form
+        new_form.appendChild(new_textarea);
+        new_form.appendChild(send_button);
+        new_form.appendChild(cancel_button);
+        form_wrapper.appendChild(new_form);
+
+        // Insert after original post
+        parent.insertBefore(form_wrapper, parent.firstElementChild.nextSibling);
+    }
+
+    if (form) {
         var container = document.getElementById('comments'),
-            form = document.getElementById('comment_form'),
-            clear = document.getElementById('form_clear'),
+            // clear = document.getElementById('form_clear'),
             header = document.getElementById('form_header'),
             parent_id = document.getElementById('parent_id');
 
-        // Reset form when cancel button is used
-        clear.addEventListener('click', function(e) {
-            e.preventDefault();
-
-            header.innerHTML = 'Leave A Comment.';
-            parent_id.setAttribute('value', '0');
-            form.reset();
-        });
-
         // Reply link functionality
         container.addEventListener('click', function(e) {
-            var target = e.target;
+            var target = e.target,
+                container = target.parentNode.parentNode.parentNode.parentNode.parentNode, // What a magical beast!!
+                reply_form = document.getElementById("comment-form-reply");
+
+            if (target.classList.contains('cs-button-cancel') && target.parentNode.parentNode === reply_form) {
+                reply_form.parentNode.removeChild(reply_form);
+            }
 
             if (target.classList.contains('reply')) {
-                var comment = target.getAttribute('data-id'),
-                    name = target.getAttribute('data-name');
-
                 e.preventDefault();
 
-                // Move to comment form
-                window.location.hash = 'comment-box';
+                // Remove existing reply form
+                if (reply_form) {
+                    reply_form.parentNode.removeChild(reply_form);
+                }
 
-                // Indicate who commenter is responding to
-                header.innerHTML = 'Reply to ' + name + ',';
+                createForm(container);
 
+                // var comment = target.getAttribute('data-id'),
+                //     name = target.getAttribute('data-name');
+                //
                 // Set parent_id for reply post
-                parent_id.setAttribute('value', comment);
+                // parent_id.setAttribute('value', comment);
             }
         });
     }
--- system/application/views/_comments.php
+++ system/application/views/_comments.php
@@ -2,20 +2,16 @@
 <div id="comment-box" class="cs-comment-box">
     <?php if ($this->session->userdata('oauth')) { ?>
         <h2 id="form_header" class="cs-comments-title">Leave A Comment.</h2>
-        <div class="cs-comment-form">
-            <?php
-                $attributes = array('id'=>'comment_form');
-                echo form_open("comment/submit/$type/$id", $attributes);
-            ?>
-            <textarea name="body" style="width:100%" rows="5" placeholder="comment here....."></textarea>
-            <input name="parent_id" value="0" id="parent_id" style="display: none;"/>
-            <input type="submit" class="cs-button cs-button-sm" value="Submit"/>
-            <a href="#" id="form_clear" class="cs-button cs-button-sm cs-button-cancel">Cancel</a>
-            <?= form_close() ?>
+        <div class="cs-comment-form cs-comment-form-main">
+            <form id="comment-form-master">
+                <textarea name="body" rows="5" placeholder="comment here....."></textarea>
+                <a href="#" id="form_clear" class="cs-button cs-button-sm">Submit</a><!--
+                --><a href="#" id="form_clear" class="cs-button cs-button-sm cs-button-cancel">Cancel</a>
+            </form>
         </div>
     <?php } else { ?>
         <h2 class="cs-comments-title">Log In To Comment!</h2>
-        <div class="cs-comment-form">
+        <div class="cs-comment-form-main">
             <div class="cs-comment-login cs-flex-row">
                 <?php $this->view('oauth/_login-button') ?>
             </div>
