7eae7cedc57abc4ff69379e50570dc25bc48daec
nyrrad <dirtbikedb@gmail.com>
2017-06-27 09:30:02 -0500

    Minify css, uglify css

--- gulpfile.js
+++ gulpfile.js
@@ -6,5 +6,5 @@ require('./styles/gulp/tasks/style')(gulp);
 require('./styles/gulp/tasks/js')(gulp);
 require('./styles/gulp/tasks/watch')(gulp);
 
-gulp.task('default', ['style:lint','style:dev', 'js', 'watch']);
-gulp.task('release', ['style:release']);
+gulp.task('default', ['style:lint','style:dev', 'js:dev', 'watch']);
+gulp.task('release', ['style:release', 'js:release']);
--- styles/gulp/tasks/js.js
+++ styles/gulp/tasks/js.js
@@ -1,16 +1,25 @@
 'use strict';
 
 var sourcemaps = require('gulp-sourcemaps'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify');
 
 const paths = require('../options/paths');
 
 module.exports = (gulp) => {
 
-    gulp.task('js', () => {
+    gulp.task('js:dev', () => {
         return gulp.src(paths.js.src)
+            .pipe(sourcemaps.init())
             .pipe(concat('index.js'))
             .pipe(sourcemaps.write('./'))
+            .pipe(gulp.dest('./'));
+    });
+
+    gulp.task('js:release', () => {
+        return gulp.src(paths.js.src)
+            .pipe(concat('index.js'))
+            .pipe(uglify())
             .pipe(gulp.dest('./'));
     });
 
