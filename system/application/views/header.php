0434c88d6f0c0b53c066c34824f63be6cea96645
nyrrad <dirtbikedb@gmail.com>
2017-06-30 17:22:31 -0500

    use $type var instead of preg_match()

--- system/application/views/header.php
+++ system/application/views/header.php
@@ -8,13 +8,13 @@
 
     <meta property="og:site_name" content="Cinnamon Spices" />
     <meta property="og:title" content="<?= ucfirst($type) ?>" />
-    <?php if (preg_match('|^/themes.*?|',$_SERVER['REQUEST_URI'])) { ?>
+    <?php if ( $type == 'themes' ) { ?>
         <meta property="og:description" content="Change the look and feel of Cinnamon with themes!" />
-    <?php } elseif (preg_match('|^/applets.*?|',$_SERVER['REQUEST_URI'])) { ?>
+    <?php } elseif ( $type == 'applets' ) { ?>
         <meta property="og:description" content="Add applets to your Cinnamon panel!" />
-    <?php } elseif (preg_match('|^/desklets.*?|',$_SERVER['REQUEST_URI'])) { ?>
+    <?php } elseif ( $type == 'desklets' ) { ?>
         <meta property="og:description" content="Add desklets on top of your desktop wallpaper!" />
-    <?php } elseif (preg_match('|^/extensions.*?|',$_SERVER['REQUEST_URI'])) { ?>
+    <?php } elseif ( $type == 'extensions' ) { ?>
         <meta property="og:description" content="Change the way Cinnamon works with extensions!" />
     <?php } ?>
     <meta property="og:type" content="website" />
@@ -34,22 +34,22 @@
     </div>
     <div class="cs-jumbotron-content cs-flex-column">
     <h1 class="cs-heading-large"><?= ucfirst($type); ?></h1>
-    <?php if (preg_match('|^/themes.*?|',$_SERVER['REQUEST_URI'])) { ?>
+    <?php if ( $type == 'themes' ) { ?>
         <p>
             Change the look and feel of Cinnamon with themes!
             <br>
             <span class="cs-jumbotron-highlight">To install a theme: Download it and decompress it in ~/.themes.</span>
-    <?php } elseif (preg_match('|^/applets.*?|',$_SERVER['REQUEST_URI'])) { ?>
+    <?php } elseif ( $type == 'applets' ) { ?>
         <p>
             Add applets to your Cinnamon panel!
             <br>
             <span class="cs-jumbotron-highlight">To install an applet: Download it and decompress it in ~/.local/share/cinnamon/applets.</span>
-    <?php } elseif (preg_match('|^/desklets.*?|',$_SERVER['REQUEST_URI'])) { ?>
+    <?php } elseif ( $type == 'desklets' ) { ?>
         <p>
             Add desklets on top of your desktop wallpaper!
             <br>
             <span class="cs-jumbotron-highlight">To install a desklet: Download it and decompress it in ~/.local/share/cinnamon/desklets.</span>
-    <?php } elseif (preg_match('|^/extensions.*?|',$_SERVER['REQUEST_URI'])) { ?>
+    <?php } elseif ( $type == 'extensions' ) { ?>
         <p>
             Change the way Cinnamon works with extensions!
             <br>
@@ -65,10 +65,10 @@
     <div class="cs-sidebar">
         <div class="title">Addons</div>
         <ul class="cs-sidebar-links">
-            <li class="<?= preg_match('|^/themes.*?|',$_SERVER['REQUEST_URI'])?'active':'' ?>"><a href="/themes">Themes</a></li>
-            <li class="<?= preg_match('|^/applets.*?|',$_SERVER['REQUEST_URI'])?'active':'' ?>"><a href="/applets">Applets</a></li>
-            <li class="<?= preg_match('|^/desklets.*?|',$_SERVER['REQUEST_URI'])?'active':'' ?>"><a href="/desklets">Desklets</a></li>
-            <li class="<?= preg_match('|^/extensions.*?|',$_SERVER['REQUEST_URI'])?'active':'' ?>"><a href="/extensions">Extensions</a></li>
+            <li class="<?= $type == 'themes' ? 'active' : '' ?>"><a href="/themes">Themes</a></li>
+            <li class="<?= $type == 'applets' ? 'active' : '' ?>"><a href="/applets">Applets</a></li>
+            <li class="<?= $type == 'desklets' ? 'active' : '' ?>"><a href="/desklets">Desklets</a></li>
+            <li class="<?= $type == 'extensions' ? 'active' : '' ?>"><a href="/extensions">Extensions</a></li>
         </ul>
     </div>
     <div class="cs-main-content">
